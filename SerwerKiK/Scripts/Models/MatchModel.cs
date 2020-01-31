023c67a636869ad7b5ba9743b71a5c363eee416f
RoksanaStanczyk <Rstanczyk24@gmail.com>
2020-01-31 17:09:16 +0100

    Wrzucony projekt serwera z obsługą klienta z kółko i krzyzyk

---
+++ SerwerKiK/Scripts/Models/MatchModel.cs
@@ -0,0 +1,130 @@
+﻿using DarkRift;
+using DarkRift.Server;
+using Scripts.Helpers;
+using Scripts.Networking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scripts.Models {
+	class MatchModel {
+
+		public enum SlateStatus {
+			NONE,
+			PLAYER1,
+			PLAYER2
+		}
+
+		private static ushort globalID = 0;
+
+		public readonly ushort id;
+		private PlayerModel player1;
+		private PlayerModel player2;
+
+		private ushort currentPlayerClientID;
+
+		public SlateStatus[] slates;
+
+		public bool MatchOver = false;
+
+		public ushort CurrentPlayerClientID {
+			get { return currentPlayerClientID; }
+		}
+
+		public MatchModel(PlayerModel player1, PlayerModel player2) {
+
+			id = ++globalID;
+
+			currentPlayerClientID = player1.Client.ID;
+			this.player1 = player1;
+			this.player2 = player2;
+
+			slates = new SlateStatus[9];
+		}
+
+	
+		public bool PlayerTakesSlate(ushort slateIndex, IClient client) {
+
+			if (currentPlayerClientID != client.ID) {
+				Console.WriteLine("Nie Twoja tura");
+				return false;
+			}
+
+			
+			if (slateIndex >= slates.Length) {
+				Console.WriteLine("Złe pole");
+				return false;
+			}
+
+			if (slates[slateIndex] != SlateStatus.NONE) {
+				Console.WriteLine("pole zajęte");
+				return false;
+			}
+
+			if (player1.Client == client) {
+				Console.WriteLine($"gracz 1 (klient-{player1.Client.ID}) zaznaczył pozycję: {slateIndex}");
+			} else if (player2.Client == client) {
+				Console.WriteLine($"gracz 2 ( klient-{player2.Client.ID}) zaznaczył pozycję: {slateIndex}");
+			}
+
+			
+			slates[slateIndex] = player1.Client == client ? SlateStatus.PLAYER1 : SlateStatus.PLAYER2;
+
+			using (DarkRiftWriter writer = DarkRiftWriter.Create()) {
+
+				ushort winnerClientID = 0;
+				bool win = MatchHelper.GetWinner(slates, player1.Client.ID, player2.Client.ID, out winnerClientID);
+				bool draw = true;
+
+				if (win == false) {
+					// spr czy pełne
+					for(int i = 0; i < slates.Length; i++) {
+						if (slates[i] == SlateStatus.NONE) {
+							draw = false;
+							break;
+						}
+					}
+				}
+
+				writer.Write(slateIndex);
+				writer.Write(client.ID);
+				Console.WriteLine($"wykonanie ruchu przez klienta o id: {client.ID}.");
+
+				if (win) {
+					writer.Write((byte)1);
+				} else if (draw) {
+					writer.Write((byte)2);
+				} else {
+					writer.Write((byte)0);
+				}
+
+				if (win) {
+					MatchOver = true;
+					writer.Write(winnerClientID);
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.DarkGreen;
+					Console.WriteLine("mamy ZWYCIĘZCĘ");
+					Console.BackgroundColor = ConsoleColor.Black;
+					Console.ForegroundColor = ConsoleColor.White;
+				} else if (draw) {
+					MatchOver = true;
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.DarkRed;
+					Console.WriteLine("REMIS");
+					Console.BackgroundColor = ConsoleColor.Black;
+					Console.ForegroundColor = ConsoleColor.White;
+				}
+				using (Message msg = Message.Create((ushort)Tags.Tag.SERVER_CONFIRM_SLATE_TAKEN, writer)) {
+					player1.Client.SendMessage(msg, SendMode.Reliable);
+					player2.Client.SendMessage(msg, SendMode.Reliable);
+				}
+
+				currentPlayerClientID = currentPlayerClientID == player1.Client.ID ? player2.Client.ID : player1.Client.ID;
+				Console.WriteLine($"kolej klienta o id: {currentPlayerClientID}");
+			}
+
+			return true;
+		}
+
+	}
+}
---
+++ SerwerKiK/SerwerPlugin.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using DarkRift;
+using DarkRift.Server;
+using Scripts.Models;
+using Scripts.Networking;
+namespace SerwerKiK
+{
+    public class SerwerPlugin : Plugin
+    {
+        public override bool ThreadSafe => false;
+        public override Version Version => new Version(0, 0, 1);
+
+        private PlayerModel pendingPlayer;
+        private Dictionary<ushort, MatchModel> matches;
+
+        public SerwerPlugin(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+
+            ClientManager.ClientConnected += OnClientConnected;
+            ClientManager.ClientDisconnected += OnClientDisconnected;
+
+            matches = new Dictionary<ushort, MatchModel>();
+        }
+
+        private void OnClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            WriteEvent("hej, " + e.Client.ID, DarkRift.LogType.Info);
+            e.Client.MessageReceived += OnClientMessageReceived;
+        }
+
+        private void OnClientDisconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+            WriteEvent("papa, " + e.Client.ID, DarkRift.LogType.Info);
+
+            if (pendingPlayer != null && pendingPlayer.Client == e.Client)
+            {
+                pendingPlayer = null;
+            }
+
+        }
+
+        private void OnClientMessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            switch (e.Tag)
+            {
+                case (ushort)Tags.Tag.SET_NAME:
+
+                    
+
+                    using (Message message = e.GetMessage()) //czytanie message
+                    {
+                        using (DarkRiftReader reader = message.GetReader())
+                        {
+                            string name = reader.ReadString();
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("witaj " + name);
+                            Console.ForegroundColor = ConsoleColor.White;
+
+                            PlayerModel newPlayer = new PlayerModel(e.Client, name);
+
+                            if (pendingPlayer == null)
+                            {
+                                // nowy gracz
+                                pendingPlayer = newPlayer;
+                            }
+                            else
+                            {
+                                //nowy meczyk
+
+                                MatchModel match = new MatchModel(pendingPlayer, newPlayer);
+                                matches.Add(match.id, match);
+
+                                
+                                using (DarkRiftWriter writer = DarkRiftWriter.Create())
+                                {
+                                    writer.Write(match.id);
+                                    writer.Write(match.CurrentPlayerClientID);
+                                    using (Message msg = Message.Create((ushort)Tags.Tag.GOT_MATCH, writer))
+                                    {
+                                        pendingPlayer.Client.SendMessage(msg, SendMode.Reliable);
+                                        newPlayer.Client.SendMessage(msg, SendMode.Reliable);
+                                    }
+                                }
+
+                                pendingPlayer = null;
+                            }
+                        }
+                    }
+                    break;
+
+                case (ushort)Tags.Tag.SLATE_TAKEN:
+
+                    using (Message message = e.GetMessage())
+                    {
+                        using (DarkRiftReader reader = message.GetReader())
+                        {
+
+                            ushort matchId = reader.ReadUInt16();
+                            ushort slateIndex = reader.ReadUInt16();
+
+                            if (matches.ContainsKey(matchId))
+                            {
+                                MatchModel match = matches[matchId];
+                                match.PlayerTakesSlate(slateIndex, e.Client);
+                                if (match.MatchOver)
+                                {
+                                    
+                                    Console.WriteLine("Koniec rozgrywki");
+                                    matches.Remove(matchId);
+                                    
+                                }
+                            }
+                        }
+                    }
+
+                    break;
+            }
+        }
+    }
+}
